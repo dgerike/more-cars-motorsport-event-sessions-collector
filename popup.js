710b45164f352475dcac382a49281c1ca1b5a9ff
dgerike <dennis.gerike@gmail.com>
2023-01-20 12:33:58 +0100

    #1 when the user selects a racing series all events belonging to this series will be loaded

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ let racingSeries = [{
     "name": "WRC",
     "more_cars_id": 311
 }]
+let apiBaseUrl = 'https://more-cars.net'
 
 renderRacingSeriesList(racingSeries)
 
@@ -14,4 +15,41 @@ function renderRacingSeriesList(seriesList) {
 
         $('#racingSeriesList').append(html)
     })
+}
+
+$('#racingSeriesList').change(function () {
+    let value = $(this).val();
+    fetchRacingEventsFromMoreCars(value);
+});
+
+function fetchRacingEventsFromMoreCars(racingSeriesId) {
+    let endpoint = '/racing-series/' + racingSeriesId + '/racing-events';
+
+    $.ajax({
+        type: 'GET',
+        url: apiBaseUrl + '/api/v1' + endpoint,
+    }).done(function (response) {
+        let renderedEventList = renderEventList(response.data)
+        $('#racingEventList').html(renderedEventList);
+    }).fail(function (response, status) {
+
+    });
+}
+
+function renderEventList(events) {
+    let html = '';
+
+    html += '<option selected disabled>--- Select Racing Event ---</option>'
+    events.forEach(event => {
+        html +=
+            '<option value="' + event.id + '">' +
+            'Round ' + event.round +
+            ': ' + event.name +
+            ' (' + event.start_date +
+            ' - ' + event.end_date +
+            ')' +
+            '</option>'
+    });
+
+    return html;
 }
