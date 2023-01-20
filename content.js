3662f4b43f02bb990aefc188576a7e0e748656ad
dgerike <dennis.gerike@gmail.com>
2023-01-20 13:41:04 +0100

    #1 selecting rows across multiple tables doesn't work in CSS -> switching to a jquery solution

--- content.js
+++ content.js
@@ -1,41 +1,13 @@
-function extractDataPoint(baseSelector, fieldName, selectors) {
+function extractDataPoint(baseSelector, rowNumber, fieldName, selectors) {
     if (!selectors[fieldName]) {
         return null
     }
 
-    if (!document.querySelector(baseSelector + ' ' + selectors[fieldName])) {
-        return null
-    }
-
-    let childNode = 0
-    if (selectors[fieldName + '_childnode']) {
-        childNode = selectors[fieldName + '_childnode']
-    }
-
-    if (!document.querySelector(baseSelector + ' ' + selectors[fieldName]).childNodes[childNode]) {
-        return null
-    }
-
-    let datapoint = document
-        .querySelector(baseSelector + ' ' + selectors[fieldName])
-        .childNodes[childNode]
-        .textContent
+    let datapoint = $(baseSelector)
+        .eq(rowNumber)
+        .find(selectors[fieldName])
+        .text()
         .trim()
-
-    if (selectors[fieldName + '_suffix']) {
-        let suffixElement = document.querySelector(baseSelector + ' ' + selectors[fieldName + '_suffix'])
-        if (suffixElement) {
-            let suffix = suffixElement.textContent
-            datapoint = datapoint + suffix
-        }
-    }
-
-    if (selectors[fieldName + '_attribute']) {
-        datapoint = document
-            .querySelector(baseSelector + ' ' + selectors[fieldName])
-            .getAttribute(selectors[fieldName + '_attribute'])
-            .trim()
-    }
 
     return datapoint
 }
@@ -55,12 +27,12 @@ function getRowNumberForCurrentPosition(pos, selectors) {
 }
 
 function extractSessionForPosition(pos, selectors) {
-    const nthChild = getRowNumberForCurrentPosition(pos, selectors)
-    const baseSelector = selectors.table_selector + ' ' + selectors.row_selector + ':nth-of-type(' + nthChild + ')'
+    const rowNumber = getRowNumberForCurrentPosition(pos, selectors)
+    const baseSelector = selectors.table_selector + ' ' + selectors.row_selector
 
-    const stage = extractDataPoint(baseSelector, 'stage', selectors)
-    const name = extractDataPoint(baseSelector, 'session_name', selectors)
-    const time = extractDataPoint(baseSelector, 'start_time', selectors)
+    const stage = extractDataPoint(baseSelector, rowNumber, 'stage', selectors)
+    const name = extractDataPoint(baseSelector, rowNumber, 'session_name', selectors)
+    const time = extractDataPoint(baseSelector, rowNumber, 'start_time', selectors)
 
     return {
         "stage": stage,
@@ -82,7 +54,7 @@ function extractAllSessions(selectors) {
         sessionsCount = Math.ceil(sessionsCount / (selectors.skip_rows + 1))
     }
 
-    for (let i = 1; i <= sessionsCount; i++) {
+    for (let i = 0; i < sessionsCount; i++) {
         let session = extractSessionForPosition(i, selectors)
 
         sessions.push(session)
